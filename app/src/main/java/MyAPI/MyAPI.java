541d731e8928e2cd84ca8959a3d36c9923af1afb
Softwarica College Lab <reshav401@gmail.com>
2019-05-22 11:57:43 +0545

    q

--- app/src/main/java/MyAPI/MyAPI.java
+++ app/src/main/java/MyAPI/MyAPI.java
@@ -3,14 +3,18 @@ package MyAPI;
 import java.util.List;
 import java.util.Map;
 
+import model.ImageResponse;
 import model.User;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface MyAPI {
 
@@ -27,4 +31,9 @@ public interface MyAPI {
     @FormUrlEncoded
     @POST("heroes")
     Call<Void> addHero2 (@FieldMap Map<String, String> map);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
 }
